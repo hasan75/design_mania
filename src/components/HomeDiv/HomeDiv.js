605440eca7c2a8637ab220e10fb2c8188e545f54
Hasan75 <hasanjab14@gmail.com>
2022-03-28 16:40:10 +0600

    divs are designed

--- src/components/HomeDiv/HomeDiv.js
+++ src/components/HomeDiv/HomeDiv.js
@@ -6,24 +6,30 @@ const HomeDiv = () => {
   return (
     <div className={`${homeDivStyles.homeDiv} container`}>
       <div className={`${homeDivStyles.rowBanckground} row g-1`}>
-        <div className='col-md-5 ps-sm-4 ps-md-2 pt-4'>
+        <div className='col-md-5 pt-4 mt-3'>
           <h1 className={`${homeDivStyles.heading} fw-bold`}>
-            Start Making Your Website Today
+            Start Making <br /> Your Website Today
           </h1>
           <h6 className='fw-bold text-secondary my-2'>From Idea To Product</h6>
           <div className='row g-1'>
             <div className='col-4'>
-              <div className={`${homeDivStyles.homeCategory}`}>
+              <div
+                className={`${homeDivStyles.homeCategory} d-flex justify-content-center align-items-center text-center`}
+              >
                 <p>Web Design and Development</p>
               </div>
             </div>
             <div className='col-4'>
-              <div className={`${homeDivStyles.homeCategory}`}>
+              <div
+                className={`${homeDivStyles.homeCategory} d-flex justify-content-center align-items-center`}
+              >
                 <p>Custom Web App</p>
               </div>
             </div>
             <div className='col-4'>
-              <div className={`${homeDivStyles.homeCategory}`}>
+              <div
+                className={`${homeDivStyles.homeCategory} d-flex justify-content-center align-items-center`}
+              >
                 <p>E-commerce Development</p>
               </div>
             </div>
