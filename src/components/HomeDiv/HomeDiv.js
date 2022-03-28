548750f8e766ea8697b14e54cdc7d1f1a2bd234c
Hasan75 <hasanjab14@gmail.com>
2022-03-28 18:32:09 +0600

    style margined

--- src/components/HomeDiv/HomeDiv.js
+++ src/components/HomeDiv/HomeDiv.js
@@ -5,34 +5,51 @@ import videoContent from '../../videos/Untitled design.mp4';
 const HomeDiv = () => {
   return (
     <div className={`${homeDivStyles.homeDiv} container`}>
-      <div className={`${homeDivStyles.rowBanckground} row g-1`}>
-        <div className='col-md-5 pt-4 mt-3'>
-          <h1 className={`${homeDivStyles.heading} fw-bold`}>
+      <div className={`${homeDivStyles.rowBanckground} row`}>
+        <div className='col-md-5 pt-4 mt-1'>
+          <h1
+            className={`${homeDivStyles.heading} fw-bold mt-lg-4 mt-sm-2 mb-3`}
+          >
             Start Making <br /> Your Website Today
           </h1>
-          <h6 className='fw-bold text-secondary my-2'>From Idea To Product</h6>
+          <h6 className='fw-bold text-secondary mt-3 mb-3 pb-lg-3'>
+            From Idea To Product
+          </h6>
           <div className='row g-1'>
             <div className='col-4'>
               <div
                 className={`${homeDivStyles.homeCategory} d-flex justify-content-center align-items-center text-center`}
               >
-                <p>Web Design and Development</p>
+                <span>Web Design and Development</span>
               </div>
             </div>
             <div className='col-4'>
               <div
                 className={`${homeDivStyles.homeCategory} d-flex justify-content-center align-items-center`}
               >
-                <p>Custom Web App</p>
+                <span>Custom Web App</span>
               </div>
             </div>
             <div className='col-4'>
               <div
                 className={`${homeDivStyles.homeCategory} d-flex justify-content-center align-items-center`}
               >
-                <p>E-commerce Development</p>
+                <span>E-commerce Development</span>
               </div>
             </div>
+          </div>
+          <div
+            className={`${homeDivStyles.getStarted} my-4 d-flex align-items-center`}
+          >
+            <button
+              className={`${homeDivStyles.getStartButton} d-flex align-items-center`}
+            >
+              <span> Get Started</span>{' '}
+              <i class='fa-solid fa-arrow-right-long ms-2'></i>
+            </button>
+            <span className={`${homeDivStyles.textLine} ms-3 ps-3`}>
+              Let Your Dreams be Live!
+            </span>
           </div>
         </div>
         <div className='col-md-7'>
